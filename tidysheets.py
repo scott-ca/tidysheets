223f3562d9d55c450b606207a17f9a59516e6d8d
scott-ca <59944183+scott-ca@users.noreply.github.com>
2022-07-24 21:48:52 -0600

    updating documentation

--- tidysheets.py
+++ tidysheets.py
@@ -8,15 +8,17 @@ import os.path
 
 """
 Version 0.0.1
--- Cleans files by removing leading/trailing spaces and forces lowercasea in all cells.
+- Cleans files by removing leading/trailing spaces and forces lowercase in all cells.
 
 USAGE
--- Run from CLI tidysheet.py along with any number of files. It will place cleaned version of those same files within a subfolder called cleaned files using the original file name.
+- Run from CLI tidysheets.py along with any number of files. It will place cleaned version of the files within a subfolder called cleaned files using the original file name keeping the original files unchanged.
+IE. python tidysheets.py file1.csv file2.csv file3.csv
 
 To DO
--- Add in functionality to process XLSX files
--- Add in optional arguements to disable certain features (IE not forcing lowercase)
--- Add in optional arguments to tidy additional data (IE remove numbers)
+- Add in functionality to process XLSX files.
+- Add in optional arguements to disable certain features (IE not forcing lowercase).
+- Add in optional arguments to tidy additional data (IE remove numbers).
+- Add in optional arguments to replace existing files.
 """
 
 def clean_spaces(dirtydf):
